4e99647d124f2a33fe3bf5a3062d26d1fe97389d
Kyaw Zayar Sein <124577297+kyawzayarseinedu@users.noreply.github.com>
2026-03-24 22:30:51 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
-import asyncio
 import run
+import asyncio
 
-async def start_app():
-
-    is_verified = await run.verify_activation()
-    if is_verified:
-        try:
-
-            await run.main_engine()
-        except KeyboardInterrupt:
-            print("\nStopped by User.")
+async def main():
+    try:
+        # Module ထဲမှာ ပါဝင်တဲ့ start_process ကို ခေါ်ယူခြင်း
+        # မှတ်ချက် - function အတွက် လိုအပ်တဲ့ arguments တွေကို ထည့်သွင်းပေးရန် လိုအပ်နိုင်ပါတယ်
+        print("Starting process...")
+        await run.start_process()
+        
+        # အခြား function များကိုလည်း လိုအပ်သလို ခေါ်ယူနိုင်ပါတယ်
+        # ဥပမာ - run.high_speed_ping()
+        
+    except Exception as e:
+        print(f"Error occurred: {e}")
 
 if __name__ == "__main__":
-    asyncio.run(start_app())
-
+    # ဤ module သည် coroutine များ (async) အသုံးပြုထားသဖြင့် asyncio နှင့် ပတ်ရပါသည်
+    asyncio.run(main())
